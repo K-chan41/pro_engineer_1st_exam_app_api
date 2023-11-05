ff45192c6e3cb89dbe8f0f8d1d8cc08fdd613620
K-chan41 <keisakata11@gmail.com>
2023-11-05 22:21:56 +0900

    create question api

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -1,11 +1,18 @@
 class Api::V1::QuestionsController < Api::V1::BaseController
   def filter
     subject_ids = params[:subject_ids]
-    # Eager loading of related models
-    questions = Question.includes(:subject, :label, :choices).where(subject_id: subject_ids)
+    questions = Question.includes(:choices, :subject, :label).where(subject_id: subject_ids)
     
-    # Use the QuestionSerializer to serialize the questions along with their related models
-    json_string = QuestionSerializer.new(questions, include: [:subject, :label, :choices]).serializable_hash.to_json
+    # Serialize questions with related choices, subjects, and labels
+    json_string = QuestionSerializer.new(questions, options).serializable_hash.to_json
     render json: json_string
+  end
+
+  private
+
+  def options
+    options = {}
+    options[:include] = [:choices, :subject, :label]
+    options
   end
 end
