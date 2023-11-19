7432ce89a6f89f749caa6e268f8535087b8fedb2
K-chan41 <keisakata11@gmail.com>
2023-11-19 16:32:10 +0900

    create new_user_register_function

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :subjects, only: [:index]
       resource :authentication, only: %i[create]
+      resource :registration, only: %i[create]
       resources :questions, only: [:index] do
         collection do
           get 'filter'
