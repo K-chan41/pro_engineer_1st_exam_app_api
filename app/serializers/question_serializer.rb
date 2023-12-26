15dbcd2717c1769b8fe6307aab76689c202382c5
K-chan41 <keisakata11@gmail.com>
2023-12-26 16:20:11 +0900

    modifiy serializer

--- app/serializers/question_serializer.rb
+++ app/serializers/question_serializer.rb
@@ -2,7 +2,7 @@ class QuestionSerializer
   include JSONAPI::Serializer
   attributes :number, :content, :question_img_src, :correct_answer_no, :commentary, :answer_img_src
 
+  has_many :choices, serializer: ChoiceSerializer
   belongs_to :subject, serializer: SubjectSerializer
   belongs_to :label, serializer: LabelSerializer, if: Proc.new { |record| record.label.present? }
-  has_many :choices, serializer: ChoiceSerializer
 end
