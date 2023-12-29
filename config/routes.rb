617df152f04c30b6aa1d786be2f028c17c217593
K-chan41 <keisakata11@gmail.com>
2023-12-29 17:00:51 +0900

    resoure registration

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :subjects, only: [:index]
       resource :authentication, only: [:create, :destroy]
-      resources :registrations, only: [:create, :destroy]
+      resource :registration, only: [:create, :destroy]
       resources :questions, only: [:index] do
         collection do
           get 'filter'
